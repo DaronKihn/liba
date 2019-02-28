7f965d1aaf7445a8a83649d2bb4afb4d9d7524fa
Daron Kihn <dkihn@e1r5p3.21-school.ru>
2019-02-28 21:42:22 +0300

    28.02.19

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: dkihn <dkihn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/20 18:48:29 by dkihn             #+#    #+#             */
-/*   Updated: 2019/02/27 17:25:07 by dkihn            ###   ########.fr       */
+/*   Updated: 2019/02/28 16:35:39 by dkihn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,19 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t size)
 	size_t	n;
 	size_t	s1;
 
+	if (size == 0)
+		return (ft_strlen(src));
 	n = 0;
 	while (*(dst + n) && (n < size))
 		n++;
 	s1 = n;
+	if (size == 0)
+		while (*(src + (n - s1)) && (n < (size)))
+		{
+			*(dst + n) = *(src + (n - s1));
+			n++;
+		}
+	else
 	while (*(src + (n - s1)) && (n < (size - 1)))
 	{
 		*(dst + n) = *(src + (n - s1));
